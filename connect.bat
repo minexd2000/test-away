a9c467506b44ca9e61e3fac4084a38abf533749f
minexd2000 <150964183+minexd2000@users.noreply.github.com>
2024-06-06 17:12:02 +0300

    Update connect.bat

--- connect.bat
+++ connect.bat
@@ -3,5 +3,4 @@ C:
 setlocal
 set PATH=%PATH%;%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion
 remoting_start_host.exe --code="4/0AdLIrYcChBVR_zY7WEwSKJ3_Wqio8Gi1ZbBlbacB_084gO1zqvegnmJw_zkEC3yRD9q23A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name="%COMPUTERNAME%"
-3020666
 endlocal
