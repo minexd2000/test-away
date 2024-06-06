689de74f77040210c9415a916c27877c6c4c5505
minexd2000 <150964183+minexd2000@users.noreply.github.com>
2024-06-06 17:42:56 +0300

    Update connect.bat

--- connect.bat
+++ connect.bat
@@ -1,6 +1,5 @@
 @echo off
 C:
 setlocal
-set PATH=%PATH%;%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion
-remoting_start_host.exe --code="4/0AdLIrYcChBVR_zY7WEwSKJ3_Wqio8Gi1ZbBlbacB_084gO1zqvegnmJw_zkEC3yRD9q23A" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name="%COMPUTERNAME%"
+set PATH=%PATH%;"%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AdLIrYcVVdUo6NYPZVpIMqSDHifQUksj2t3Bsa8caitATuRvjHfvaDwurkFRFvUrS0k8uQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=%COMPUTERNAME%
 endlocal
